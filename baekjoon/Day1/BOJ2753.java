defe91d0220205fc4286d809b336598cd2ca4ec8
Daegyu Han <bigstar77han@gmail.com>
2025-01-07 22:32:15 +0900

    baekjoon day1-5

---
+++ baekjoon/Day1/BOJ2753.java
@@ -0,0 +1,18 @@
+package baekjoon.Day1;
+
+import java.util.Scanner;
+
+public class BOJ2753 {
+    // 윤년
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N%4 == 0 & N%100!=0) {
+            System.out.println("1");
+        } else if (N%400==0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
