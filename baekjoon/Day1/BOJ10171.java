7e83cc957f6bfe6683a3d6402fed59b51ef041ec
Daegyu Han <bigstar77han@gmail.com>
2025-01-07 22:20:39 +0900

    baekjoon day1-4

---
+++ baekjoon/Day1/BOJ10171.java
@@ -0,0 +1,11 @@
+package baekjoon.Day1;
+
+public class BOJ10171 {
+    // 고양이
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
