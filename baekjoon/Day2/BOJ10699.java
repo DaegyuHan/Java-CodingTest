6af71537e7cfd02070bfbf4336d64b272c3abee0
Daegyu Han <bigstar77han@gmail.com>
2025-01-08 19:48:41 +0900

    baekjoon day2-18

---
+++ baekjoon/Day2/BOJ10699.java
@@ -0,0 +1,13 @@
+package baekjoon.Day2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BOJ10699 {
+    // 오늘 날짜
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        System.out.println(simpleDateFormat.format(date));
+    }
+}
---
+++ baekjoon/Day2/BOJ2738.java
@@ -0,0 +1,25 @@
+package baekjoon.Day2;
+
+import java.util.Scanner;
+
+public class BOJ2738 {
+    // 행렬 덧셈
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int A[][] = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                A[i][j] += sc.nextInt();
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
