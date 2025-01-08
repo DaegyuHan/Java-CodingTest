c73b113a7749ea7c03d7118b04f9c9f9f0ab6cd9
Daegyu Han <bigstar77han@gmail.com>
2025-01-08 17:20:07 +0900

    baekjoon day2-15

---
+++ baekjoon/Day2/BOJ2475.java
@@ -0,0 +1,17 @@
+package baekjoon.Day2;
+
+import java.util.Scanner;
+
+public class BOJ2475 {
+    // 검증 수
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A[] = new int[5];
+        int sum = 0;
+        for (int i=0; i<5; i++) {
+            A[i] = sc.nextInt();
+            sum = sum + A[i] * A[i];
+        }
+        System.out.println(sum%10);
+    }
+}
---
+++ baekjoon/Day2/BOJ2743.java
@@ -0,0 +1,12 @@
+package baekjoon.Day2;
+
+import java.util.Scanner;
+
+public class BOJ2743 {
+    // 단어 길이 재기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        System.out.println(text.length());
+    }
+}
