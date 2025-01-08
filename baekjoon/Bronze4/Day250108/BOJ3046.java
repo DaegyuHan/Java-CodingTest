cb258f6b66d45cb32ea69ae7ea863c7253ebb499
Daegyu Han <bigstar77han@gmail.com>
2025-01-08 22:49:34 +0900

    baekjoon day2-5

---
+++ baekjoon/Bronze4/Day250108/BOJ3046.java
@@ -0,0 +1,13 @@
+package baekjoon.Bronze4.Day250108;
+
+import java.util.Scanner;
+
+public class BOJ3046 {
+    // R2
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        System.out.println(S*2-R1);
+    }
+}
