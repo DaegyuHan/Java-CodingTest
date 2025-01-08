56d3b6202f192d8d8ce2355b2b31a3c94866783a
Daegyu Han <bigstar77han@gmail.com>
2025-01-08 20:17:57 +0900

    baekjoon day2-20

---
+++ baekjoon/Day2/BOJ4101.java
@@ -0,0 +1,20 @@
+package baekjoon.Day2;
+
+import java.util.Scanner;
+
+public class BOJ4101 {
+    // 크냐?
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A == 0 && B == 0) break;
+            if (A>B) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
