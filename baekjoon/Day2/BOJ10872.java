66193d79df9d2e69cbf747d560f03fd3ed7476e5
Daegyu Han <bigstar77han@gmail.com>
2025-01-08 16:51:49 +0900

    baekjoon day2-11

---
+++ baekjoon/Day2/BOJ10872.java
@@ -0,0 +1,16 @@
+package baekjoon.Day2;
+
+import java.util.Scanner;
+
+public class BOJ10872 {
+    // 팩토리얼
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int answer = 1;
+        for (int i=1; i<=N; i++) {
+            answer = answer * i;
+        }
+        System.out.println(answer);
+    }
+}
---
+++ baekjoon/Day2/BOJ2558.java
@@ -0,0 +1,13 @@
+package baekjoon.Day2;
+
+import java.util.Scanner;
+
+public class BOJ2558 {
+    // A+B -2
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
+    }
+}
