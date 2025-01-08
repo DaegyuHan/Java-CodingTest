01d81d62fe9487a97fc8803158c7b40c955fb986
Daegyu Han <bigstar77han@gmail.com>
2025-01-08 16:52:01 +0900

    baekjoon day2-12

---
+++ baekjoon/Day2/BOJ7287.java
@@ -0,0 +1,9 @@
+package baekjoon.Day2;
+
+public class BOJ7287 {
+    // 등록
+    public static void main(String[] args) {
+        System.out.println(30);
+        System.out.println("bigstarhan33");
+    }
+}
