f0e9bd252156356ff011b231e149ca9f30984adc
Daegyu Han <bigstar77han@gmail.com>
2025-01-30 20:19:57 +0900

    baekjoon Day250130-1

---
+++ baekjoon/Bronze2/Day250130/BOJ1152.java
@@ -0,0 +1,15 @@
+package baekjoon.Bronze2.Day250130;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1152 {
+    // 단어의 개수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(st.countTokens());
+    }
+}
---
+++ baekjoon/Bronze2/Day250130/BOJ3052.java
@@ -0,0 +1,18 @@
+package baekjoon.Bronze2.Day250130;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BOJ3052 {
+    // 나머지
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Set<Integer> mySet = new HashSet<>();
+        for (int i=0; i<10; i++) {
+            int temp = sc.nextInt()%42;
+            mySet.add(temp);
+        }
+        System.out.println(mySet.size());
+    }
+}
