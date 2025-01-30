ada59ee5a25d6ed0a7d093ec55275978dce67ef0
Daegyu Han <bigstar77han@gmail.com>
2025-01-30 21:40:07 +0900

    baekjoon Day250130-2

---
+++ baekjoon/Bronze2/Day250130/BOJ10809.java
@@ -0,0 +1,24 @@
+package baekjoon.Bronze2.Day250130;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10809 {
+    // 알파벳 찾기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] text = br.readLine().toCharArray();
+
+        for(int i=0; i<26; i++) {
+            int temp = -1;
+            for (int j=0; j<text.length; j++) {
+                if (text[j]-'a'==i) {
+                    temp = j;
+                    break;
+                }
+            }
+            System.out.print(temp + " ");
+        }
+    }
+}
